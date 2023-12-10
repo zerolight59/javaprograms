bd5273e6e72ec9cfbaae195a9d250282aefdce88
MANU SANKAR U <115739592+zerolight59@users.noreply.github.com>
2023-12-11 00:09:21 +0530

    product of two numbers
    recived as arguments from cmd

---
+++ TwoNumProd.java
@@ -0,0 +1,9 @@
+class TwoNumProd{
+	public static void main(String[] args) {
+		int a,b;
+		System.out.println("By : MANU SANKAR U  ROLL NO:31");
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		System.out.println("Product of "+ a +" and " +b+" is "+(a*b));
+	}
+}
